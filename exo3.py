c8e0f9fc58dd2988ee4390f803d26b7dc29bf847
olidesmarais <71419150+olidesmarais@users.noreply.github.com>
2025-09-10 19:44:54 -0400

    exo 3 complété

--- exo3.py
+++ exo3.py
@@ -16,10 +16,19 @@ Prompts EXACTS à utiliser quand vous implémenterez input :
 
 
 # TODO: Lire distance et attente via input (prompts EXACTS)
-
+distance = float(input("Entrez la distance jusqu'à la destination (en kilomètres) : "))
+attente = float(input("Entrez le temps d'attente avant le prochain bus (en minutes) : "))
 
 # TODO: Calcul temps_marche et temps_bus (en minutes)
-
+temps_marche = distance * 60 / 5
+temps_bus = attente + distance * 60 / 20
 
 # TODO: Comparaison et affichage de la phrase EXACTE (une seule)
+if temps_bus < temps_marche:
+    print("Il est plus rapide de prendre le bus.")
+elif temps_bus > temps_marche:
+    print("Il est plus rapide de marcher.")
+else:
+    print("Les deux options prennent le même temps.")
+    
 
