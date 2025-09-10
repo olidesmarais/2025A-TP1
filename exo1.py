03b278f5ba017ee17051a326dab279550103987a
Nickode551 <chamberland.nicolas15@gmail.com>
2025-09-10 16:25:01 -0400

    exo 1 complété
    L'exo 1 est complété.

--- exo1.py
+++ exo1.py
@@ -14,14 +14,15 @@ Prompts (à utiliser quand vous implémenterez input) :
 
 
 # TODO: Lire le nom complet (str) avec le prompt EXACT
-
+nom_complet = input("Veuillez entrer votre nom complet : ")
 
 # TODO: Lire le nombre de déplacements par semaine (int) avec le prompt EXACT
-
+nbre_déplacements = int(input("Veuillez entrer le nombre de déplacements par semaine : "))
 
 # TODO: Calcul des déplacements annuels
-
+déplacements_annuels = int(nbre_déplacements*52)
 
 # TODO: Affichage des deux lignes EXACTES
-
+print("Bonjour " + nom_complet)
+print("Vous effectuez environ " + str(déplacements_annuels) + " déplacements par an sur le réseau STM.")
 
